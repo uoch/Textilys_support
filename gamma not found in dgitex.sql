9d62ba936f67ee48086fcdf58f54d1d5a8ef10be
THAMER CHAOUECH <thamerc@isasbs.com>
2026-04-20 14:45:52 +0100

    tickts

---
+++ gamma not found in dgitex.sql
@@ -0,0 +1,11 @@
+UPDATE trc_dtx t
+SET t.dtx_sent = 0
+WHERE t.dtx_table = 'TGL'
+  AND EXISTS (
+    SELECT 1
+    FROM tgl tg
+    WHERE tg.tgl_nomgamme = t.dtx_pk2
+      AND tg.tgl_nomgamme = '3308'
+      AND tg.tgl_tie_cod = t.dtx_pk1
+      AND tg.tgl_tie_cod = '0137'
+  );
